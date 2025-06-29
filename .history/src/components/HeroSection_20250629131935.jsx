13c8ee0e5f346556cd82099a558a503fcc960ed0
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-06-29 13:24:07 +0300

    feat(heroSection):add scroll part into hero section

--- .history/src/components/HeroSection_20250629131935.jsx
+++ .history/src/components/HeroSection_20250629131935.jsx
@@ -1,4 +1,3 @@
-import { ArrowDown } from 'lucide-react'
 import React from 'react'
 
 const HeroSection = () => {
@@ -23,8 +22,7 @@ const HeroSection = () => {
         </div>
 
         <div className='absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'>
-        <span className='text-sm text-muted-forgeround mb-2'> Scroll</span>
-        <ArrowDown className='h-5 w-5 text-primary'/>
+
         </div>
     </section>
   )
--- .history/src/components/HeroSection_20250629132207.jsx
+++ .history/src/components/HeroSection_20250629132207.jsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
         </div>
 
         <div className='absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce'>
-        <span className='text-sm text-muted-forgeround mb-2'> Scroll</span>
+        <span className='text-sm text-muted-forgeround'> Scroll</span>
         <ArrowDown className='h-5 w-5 text-primary'/>
         </div>
     </section>
