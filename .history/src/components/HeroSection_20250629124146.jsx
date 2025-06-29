48335b4affaba618b0d422cf67f1fa446f06ee60
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-06-29 13:18:18 +0300

    feat(heroSection):complete the hero-section

---
+++ .history/src/components/HeroSection_20250629124146.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className='relative min-h-screen flex flex-col items-center justify-center px-4'>
+
+    </section>
+  )
+}
+
+export default HeroSection
---
+++ .history/src/components/HeroSection_20250629124240.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className='relative min-h-screen flex flex-col items-center justify-center px-4'>
+        <div className='container max-w-4xl mx-auto text-center z-10'>
+            <div className='space-y-6'>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
